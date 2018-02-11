dfdc6d6377500620b69ab831d0a7bf590d2af190
Jean-Yves Vinet <jyvinet@hotmail.ca>
2018-02-11 10:43:54 -0400

    Update helper.py

--- rest/helper.py
+++ rest/helper.py
@@ -32,13 +32,3 @@ class Helper:
         if not date_str:
             return datetime.today().date()
         return datetime.strptime(date_str, the_format).date()
-
-    '''
-curl -d '{"id":"vinej", "name":"jean"}' -H "Content-Type: application/json" -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MTgzMjI0MzAsImlhdCI6MTUxODMyMTkzMCwic3ViIjoiNWE3ZmIwNmY4YmUxZjVjNmM3ZTc3MDMyIn0.Hi0EI6crSz5QuJMEGMi4yR0ijZOQRvz--314g00VtL0" -X GET http://localhost:8000/api/companies
-        curl -d '{"id":"vinej", "name":"jean 23"}' -H "Content-Type: application/json" 
-        -X PUT http://localhost:8000/api/companies
-        
-        curl -d '{"id":"vinej", "email":"jyvinet@hotmail.ca", "password":"123456"}' -H "Content-Type: application/json" -X POST http://localhost:8000/auth/login
-
-"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MTgzMjA4NTMsImlhdCI6MTUxODMyMDg0OCwic3ViIjoiNWE3ZmIwN
-    '''
